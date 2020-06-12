24a2078c5596f278220ecf3d7fc12e347e63d2e5
Diego Fernando Kim <diego.f.kim@gmail.com>
2020-06-11 23:39:09 -0300

    database from process (#16)

--- configs/prod.js
+++ configs/prod.js
@@ -11,7 +11,7 @@ module.exports = {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: 'users_service'
+      database: process.env.DATABASE
     }
   }
 };
